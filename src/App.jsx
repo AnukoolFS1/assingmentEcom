412fc1dd5c2091cb57c4db552c26d3c7d18d91c1
Anukool Singh Chauhan <ascmailing0@gmail.com>
2025-04-12 14:24:45 +0530

    entry point

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import { Provider } from "react-redux"
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-  <div className=''>
-    ddd
-  </div>
+    <Provider>
+      <Router>
+        <Routes>
+          <Route />
+        </Routes>
+      </Router>
+    </Provider>
   )
 }
 
