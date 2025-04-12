e08bb8f9a615041e3b268aa90cea3d2b723eb93f
Anukool Singh Chauhan <ascmailing0@gmail.com>
2025-04-12 15:10:03 +0530

    entry point has been done

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import { Provider } from "react-redux"
 import Entrypoint from "./component/Entrypoint"
+import store from "./app/store"
 
 function App() {
 
   return (
-    <Provider>
+    <Provider store={store}>
       <Router>
         <Routes>
           <Route path="/" element={<Entrypoint />} />
--- src/component/Entrypoint.jsx
+++ src/component/Entrypoint.jsx
@@ -2,11 +2,21 @@ const Entrypoint = () => {
     const backgroundImage = 'https://images.pexels.com/photos/30081518/pexels-photo-30081518/free-photo-of-serene-greenhouse-with-lush-botanical-pathway.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
 
     return (
-        <section
-            className="h-screen w-screen"
-            style={{ backgroundImage: `url(${backgroundImage})` }}>
-            asd
+        <section className="h-screen w-screen p-5 bg-green-500">
 
+            <section
+                className="h-full py-10 lg:px-20 sm:px-10 px-2 custom-bg text-[white] rounded-xl flex flex-col justify-between items-center"
+                style={{ backgroundImage: `linear-gradient(to bottom,#00000080,#00000060) ,url(${backgroundImage})` }}
+            >
+                <h1 className="text-[clamp(1.2rem,3vw,5rem)] mt-10 self-end text-animation-up">Plants-Care Products & Plants</h1>
+
+                <p className="text-[clamp(.81rem,1.2vw,5rem)] text-animation-up">Nature gives you the confidence you deserve</p>
+
+                <div className="flex flex-col justify-center gap-1">
+                    <button className="bg-indigo-500 rounded-xl py-1">Let's Get Started</button>
+                    <p>Already have an acount? <a href="#" className="underline font-semibold">Sign In</a></p>
+                </div>
+            </section>
         </section>
     )
 }
